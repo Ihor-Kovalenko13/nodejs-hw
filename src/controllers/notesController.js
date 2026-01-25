397e1a4869e81a0b3ba1d99e06aea6a3bedad107
Igor <ihor.kovalenko.01@gmail.com>
2026-01-25 16:27:55 +0100

    Add missing notes controllers

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,18 +3,11 @@ import { Note } from '../models/note.js';
 export const getAllNotes = async (req, res, next) => {
   try {
     const { page = 1, perPage = 10, tag, search } = req.query;
-
     const skip = (page - 1) * perPage;
 
     const filter = {};
-
-    if (tag) {
-      filter.tag = tag;
-    }
-
-    if (search) {
-      filter.$text = { $search: search };
-    }
+    if (tag) filter.tag = tag;
+    if (search) filter.$text = { $search: search };
 
     const [totalNotes, notes] = await Promise.all([
       Note.countDocuments(filter),
@@ -28,6 +21,43 @@ export const getAllNotes = async (req, res, next) => {
       totalPages: Math.ceil(totalNotes / perPage),
       notes,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getNoteById = async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({ message: 'Note not found' });
+    }
+
+    res.json(note);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createNote = async (req, res, next) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(201).json(note);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteNote = async (req, res, next) => {
+  try {
+    const note = await Note.findByIdAndDelete(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({ message: 'Note not found' });
+    }
+
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
