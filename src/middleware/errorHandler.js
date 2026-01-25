7e82302a11d2a49c2b98993f86f5226405bfd5c1
Igor <ihor.kovalenko.01@gmail.com>
2026-01-25 14:28:13 +0100

    fix: correct errorHandler signature with next param

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 import { HttpError } from "http-errors";
 
-export const errorHandler = (err, req, res) => {
+export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message || err.name,
